4c3c6d3b0d10682867367c6073f4cdfa5bde8a53
ashish6344 <aryankm6344@gmail.com>
2025-06-02 00:38:57 +0530

    Update offer18_webhook.py

--- offer18_webhook.py
+++ offer18_webhook.py
@@ -28,7 +28,7 @@ def handle_postback():
 
     url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
     data = {
-        "chat_id": TELEGRAM_CHANNEL,
+        "chat_id": @conversionalert,
         "text": message,
         "parse_mode": "Markdown"
     }
